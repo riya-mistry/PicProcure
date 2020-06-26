4af3dc02582c185a1dfae336508afce98c637dc7
Ashutosh Gupta <ashutoshg66@gmail.com>
2020-06-27 01:25:59 +0530

    login

--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render,HttpResponse
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.template.context_processors import csrf 
 from .models import Users
 from PicProcure.custom_azure import AzureMediaStorage
 # Create your views here.
@@ -18,4 +22,30 @@ def register(request):
             md.location= "Profile_Pics"
             pp = md.save(user.first_name + user.last_name,file)
         user.save()
+        render(request,'users/login.html')
     return render(request,'users/signup.html')
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request,'users/login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    try:
+        user= Users.objects.get(user_name=username,password=password)
+        #user= Users.objects.raw('Select * from users_users where user_name=%s and password=%s',[username],[password])
+        #user = auth.authenticate(user_name=username, password=password)
+        if user is not None:
+            #auth.login(request, user)
+            return render(request,'uploadFiles/base.html', {"full_name": user.first_name})
+
+        else:
+            return render(request,'users/login.html', {"Invalid_msg": "Invalid Username or Password"})
+    except:
+        return render(request,'users/login.html', {"Invalid_msg": "Invalid Username or Password"})
+
+def logout(request):
+    #auth.logout(request)
+    return render(request,'users/login.html')
