a7e6e0f86c4cd2134f8c90175deb562a7adcdd9e
Ashutosh Gupta <ashutoshg66@gmail.com>
2020-07-01 19:29:57 +0530

    footer

--- PicProcure/settings.py
+++ PicProcure/settings.py
@@ -87,7 +87,12 @@ DATABASES = {
         'PORT':'3306',
     }
 }
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'picprocure@gmail.com'
+EMAIL_HOST_PASSWORD = 'ashsamriya'
+EMAIL_PORT = 25
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
